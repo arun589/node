eae7d63f40b260926e3f7aab1e7ce40ab2e25bf7
arun589 <83627958+arun589@users.noreply.github.com>
2023-07-02 14:32:47 +0530

    project1

---
+++ project1.js
@@ -0,0 +1,37 @@
+const http=require("http");
+const fs=require("fs");
+const server=http.createServer((req,res)=>{
+    const url=req.url;
+    const method=req.method;
+    if(url==='/'){
+        fs.readFile("message.txt",{encoding:"UTF-8"},(err,data)=>{
+            if(err){
+                console.log(err);
+            }
+            res.write("<html>");
+            res.write(`<body>${data}</body>`);
+            res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></form><body>');
+            res.write("</html>");
+             res.end();
+        });
+    }
+    if(url==="/message" && method==="POST")
+    {
+        const body=[];
+        req.on("data",(chunk)=>{
+            body.push(chunk);
+        })
+        return req.on("end",()=>{
+            const parsedbody=Buffer.concat(body).toString();
+            const message=parsedbody.split("=")[1];
+            fs.writeFileSync("message.txt",message);
+            res.statusCode=302;
+            res.setHeader("location","/");
+            return res.end();
+        })
+       
+    }
+   
+    
+})
+server.listen(4000);
