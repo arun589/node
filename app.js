d74395366bdd3158b5a6d3de67e59f64a688b0f8
arun589 <83627958+arun589@users.noreply.github.com>
2023-07-09 12:56:54 +0530

    express second task body-parser

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express=require('express');
 const app=express();
-app.use((req,res,next)=>{
-    console.log("from express middleware");
-    next();
+const bodyparser=require(`body-parser`);
+app.use(bodyparser.urlencoded({extended:true}));
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">submit</button></form>');
 })
-app.use((req,res,next)=>{
-    console.log("from middleware 2");
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect("/");
+})
+app.use('/',(req,res,next)=>{
+    
     res.send('<h1>from express middleware</h1>');
 })
 app.listen(3000);
