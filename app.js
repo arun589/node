420926512ec22fea02ff470590d6600bc0f63610
arun589 <83627958+arun589@users.noreply.github.com>
2023-07-10 11:39:40 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const express=require('express');
 const app=express();
 const bodyparser=require(`body-parser`);
+const adminrouter=require("./routes/admin");
+const shoprouter=require("./routes/shop");
 app.use(bodyparser.urlencoded({extended:true}));
-app.use('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">submit</button></form>');
-})
-app.use('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect("/");
-})
-app.use('/',(req,res,next)=>{
-    
-    res.send('<h1>from express middleware</h1>');
+
+app.use("/admin",adminrouter);
+app.use("/shop",shoprouter);
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>page not found</h1>');
 })
 app.listen(3000);
