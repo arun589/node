660c784e8c041840f59de5e43bb4a5de824d9f21
arun589 <83627958+arun589@users.noreply.github.com>
2023-07-16 17:52:29 +0530

    model

--- product.js
+++ product.js
@@ -1,10 +1,12 @@
-const path=require('path');
-const rootDir=require('../util/path');
-exports.getcontact=(req, res, next) => {
-    res.sendFile(path.join(rootDir,'views','contact.html'));
-}
-exports.postcontact= (req, res, next) => {
-    console.log(req.body);
-    res.send('<h1>form succesfully filled');
-    
+const product=[];
+module.exports=class products{
+    constructor(t){
+        this.title=t;
+    }
+    save(){
+        product.push(this.title);
+    }
+    static fetchAll(){
+        return product;
+    }
 }
---
+++ products.js
@@ -0,0 +1,29 @@
+
+
+exports.getAddProduct = (req, res, next) => {
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  const products=new products(req.body.title);
+  products.save();
+  res.redirect('/');
+};
+
+exports.getProducts = (req, res, next) => {
+  const products=products.fetchAll();
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
+};
